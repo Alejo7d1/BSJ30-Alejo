d7c2776b6a57f1b0c00c0eae3fa8570489b0342c
Alejo7d1 <alejo7d1@gmail.com>
2025-08-16 19:58:19 -0600

    Add: Ejercicio 2 y 3

--- Tareas/POO typescript/main.js
+++ Tareas/POO typescript/main.js
@@ -21,7 +21,78 @@ var CabezeraPagina = /** @class */ (function () {
     };
     return CabezeraPagina;
 }());
+//Caso de Uso
 var paginaBanco = new CabezeraPagina("Banco Terobo", "Negro", "ComicSans");
 console.log(paginaBanco.getPropiedades());
 paginaBanco.setAlineacion("Centrado");
 paginaBanco.imprimirPropiedades();
+//Ejercicio 2
+var Calculadora = /** @class */ (function () {
+    function Calculadora(valorAPm, valorBPm) {
+        this.valorA = valorAPm;
+        this.valorB = valorBPm;
+    }
+    Calculadora.prototype.sumar = function () {
+        return (this.valorA + this.valorB);
+    };
+    Calculadora.prototype.restar = function () {
+        return (this.valorA - this.valorB);
+    };
+    Calculadora.prototype.multiplicar = function () {
+        return (this.valorA * this.valorB);
+    };
+    Calculadora.prototype.dividir = function () {
+        if (this.valorB == 0)
+            return NaN; //No se puede dividir entre 0
+        return (this.valorA / this.valorB);
+    };
+    Calculadora.prototype.potencia = function () {
+        return (Math.pow(this.valorA, this.valorB));
+    };
+    Calculadora.prototype.factorial = function () {
+        if (this.valorA < 0)
+            return NaN; //No existe factorial de negativos
+        if (this.valorA === 0 || this.valorA === 1)
+            return 1; //Caso 0 y 1 es 1
+        var resultado = 1;
+        for (var i = 2; i <= this.valorA; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    };
+    return Calculadora;
+}());
+//Caso de uso
+var calc = new Calculadora(7, 5);
+console.log(calc.sumar());
+console.log(calc.restar());
+console.log(calc.multiplicar());
+console.log(calc.dividir());
+console.log(calc.potencia());
+console.log(calc.factorial());
+//Ejercicio 3
+var Cancion = /** @class */ (function () {
+    //Metodos
+    function Cancion(titulo, genero) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.autor = "";
+    }
+    Cancion.prototype.setAutor = function (autor) {
+        this.autor = autor;
+    };
+    Cancion.prototype.getAutor = function () {
+        return this.autor;
+    };
+    Cancion.prototype.mostrarDatos = function () {
+        console.log('Título ', this.titulo);
+        console.log('Género: ', this.genero);
+        console.log('Autor: ', this.autor);
+    };
+    return Cancion;
+}());
+//Caso de uso
+var cancion1 = new Cancion("Desde el rio", "Rock");
+cancion1.setAutor("Pato Rockero");
+console.log(cancion1.getAutor());
+cancion1.mostrarDatos();
--- Tareas/POO typescript/main.ts
+++ Tareas/POO typescript/main.ts
@@ -34,3 +34,78 @@ class CabezeraPagina{
     paginaBanco.imprimirPropiedades();
 
 //Ejercicio 2
+class Calculadora{
+    private valorA:number;
+    private valorB:number;
+
+    constructor(valorAPm:number,valorBPm:number){
+        this.valorA=valorAPm;
+        this.valorB=valorBPm;
+    }
+
+    sumar(): number{
+        return (this.valorA + this.valorB);
+    }
+    restar(): number{
+        return (this.valorA - this.valorB);
+    }
+    multiplicar(): number{
+        return (this.valorA * this.valorB);
+    }
+    dividir(): number{
+        if(this.valorB == 0) return NaN; //No se puede dividir entre 0
+        return (this.valorA / this.valorB);
+    }
+    potencia(): number{
+        return (this.valorA ** this.valorB);
+    }
+    factorial(): number{
+    if (this.valorA < 0) return NaN; //No existe factorial de negativos
+    if (this.valorA === 0 || this.valorA === 1) return 1; //Caso 0 y 1 es 1
+    let resultado = 1;
+    for (let i = 2; i <= this.valorA; i++) {
+        resultado *= i;
+    }
+    return resultado;
+    }
+}
+//Caso de uso
+const calc = new Calculadora(7, 5);
+console.log(calc.sumar());
+console.log(calc.restar());
+console.log(calc.multiplicar());
+console.log(calc.dividir());
+console.log(calc.potencia());
+console.log(calc.factorial());
+
+//Ejercicio 3
+class Cancion{
+    titulo:string;
+    genero:string;
+    private autor:string;
+
+
+//Metodos
+     constructor(titulo: string, genero: string) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.autor = "";
+    }
+
+    setAutor(autor: string): void {
+        this.autor = autor;
+    }
+    getAutor(): string {
+        return this.autor;
+    }
+    mostrarDatos(): void {
+        console.log('Título ',this.titulo);
+        console.log('Género: ', this.genero);
+        console.log('Autor: ',this.autor);
+    }
+}
+//Caso de uso
+const cancion1 = new Cancion("Desde el rio", "Rock");
+cancion1.setAutor("Pato Rockero");
+console.log(cancion1.getAutor());
+cancion1.mostrarDatos();
